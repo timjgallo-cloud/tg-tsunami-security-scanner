31d60f5891b8bb8b8aeef87641af3707cf99127a
TimJGallo <timjgallo@google.com>
2026-05-18 11:11:51 -0700

    feat: add imports.tf to automatically import pre-existing GCP resources

---
+++ terraform/imports.tf
@@ -0,0 +1,29 @@
+import {
+  to = google_storage_bucket.scan_results
+  id = "threat-vulnerability-analyzer-tsunami-results"
+}
+
+import {
+  to = google_artifact_registry_repository.tsunami_repo
+  id = "projects/threat-vulnerability-analyzer/locations/us-central1/repositories/tsunami-repo"
+}
+
+import {
+  to = google_service_account.scanner_sa
+  id = "projects/threat-vulnerability-analyzer/serviceAccounts/tsunami-scanner-sa@threat-vulnerability-analyzer.iam.gserviceaccount.com"
+}
+
+import {
+  to = google_service_account.web_ui_sa
+  id = "projects/threat-vulnerability-analyzer/serviceAccounts/tsunami-web-ui-sa@threat-vulnerability-analyzer.iam.gserviceaccount.com"
+}
+
+import {
+  to = google_service_account.pubsub_sa
+  id = "projects/threat-vulnerability-analyzer/serviceAccounts/tsunami-pubsub-sa@threat-vulnerability-analyzer.iam.gserviceaccount.com"
+}
+
+import {
+  to = google_pubsub_topic.scan_completed
+  id = "projects/threat-vulnerability-analyzer/topics/tsunami-scan-completed"
+}
